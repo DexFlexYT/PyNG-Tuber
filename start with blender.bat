8bea1043445e928d57e44886ec2d9da943e5d86c
DexFlex_YT <142329830+DexFlexYT@users.noreply.github.com>
2025-11-05 23:13:55 +0400

    freshened up, added recorder, fixed start script, added one for only py, re-recorded happy

---
+++ start with blender.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" py -3.11 main.py
+
+start "" "D:\BlenderFoundation\GooEngine\goo-engine-experimental-4_3\blender-launcher.exe" "D:\code\python\PyNG-Tuber\VTuber GooEngine.goo"
--- start.bat
+++ start.bat
@@ -1,6 +1,2 @@
-@echo off
-start "" py -3.11 main.py
-
-start "" "D:\BlenderFoundation\GooEngine\goo-engine-experimental-4_3\blender-launcher.exe" "D:\code\python\PyNG-Tuber\VTuber GooEngine.goo"
-
+py -3.11 main.py
 pause
